4770019840a637e7bb925cef0085b26a649c701f
coolluucaas <lucaspsanta@gmail.com>
2021-11-07 11:12:57 -0300

    + 'userController'

---
+++ src/controller/userController.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from 'express'
+
+const users = [{ name: 'Lucas', email: 'lucas@email.com' }]
+
+class userController {
+    async handle(req: Request, res: Response) {
+        return res.send(users)
+    }
+}
+
+export { userController }
